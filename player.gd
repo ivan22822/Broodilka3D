30a9ebb168dc8a12f6365570587eb3a54e097bad
Ivan <time.never.three@gmail.com>
2026-07-06 23:43:02 +0200

    v1
    сделал всю основу

---
+++ player.gd
@@ -0,0 +1,68 @@
+extends CharacterBody3D
+
+signal hit
+
+@export var speed = 14
+@export var fall_acceleration = 75
+@export var jump_impulse = 20
+@export var bounce_impulse = 16
+
+var target_velocity = Vector3.ZERO
+
+func _physics_process(delta: float) -> void:
+	var direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.basis = Basis.looking_at(direction)
+		
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+	
+	if not is_on_floor():
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+	
+	if is_on_floor() and Input.is_action_pressed("jump"):
+		target_velocity.y = jump_impulse
+	velocity = target_velocity
+	move_and_slide()
+	
+	# Iterate through all collisions that occurred this frame
+	for index in range(get_slide_collision_count()):
+		# We get one of the collisions with the player
+		var collision = get_slide_collision(index)
+
+		# If there are duplicate collisions with a mob in a single frame
+		# the mob will be deleted after the first collision, and a second call to
+		# get_collider will return null, leading to a null pointer when calling
+		# collision.get_collider().is_in_group("mob").
+		# This block of code prevents processing duplicate collisions.
+		if collision.get_collider() == null:
+			continue
+
+		# If the collider is with a mob
+		if collision.get_collider().is_in_group("mob"):
+			var mob = collision.get_collider()
+			# we check that we are hitting it from above.
+			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+				# If so, we squash it and bounce.
+				mob.squash()
+				target_velocity.y = bounce_impulse
+				# Prevent further duplicate calls.
+				break
+
+func die():
+	hit.emit()
+	queue_free()
+	
+func _on_mob_detector_body_entered(body: Node3D) -> void:
+	die()
